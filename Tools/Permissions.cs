4e80c2c6eab418cfbe2dd1fdbf5999dc146e7ccf
Hayden <hayden@synhayden.com>
2025-01-10 21:25:10 -0700

    Update Permissions.cs
    Switched to Dictionary <- List
    Removed Serializable User Class
    Added UserPermissionLevel Check

--- Tools/Permissions.cs
+++ Tools/Permissions.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DSharpPlus.SlashCommands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,24 +10,27 @@ namespace ForgeBot.Tools
 {
     public class Permissions
     {
-        public static List<User> Users = new();
+        public static Dictionary<ulong, Permission> Users = new();
         public Permissions()
         {
-            Users.Add(new User(269626330755104769, Permission.SUDO));
+            Users.Add(269626330755104769, Permission.SUDO);
         }
 
-        [System.Serializable]
-        public class User
+        static public Permission GetUserLevel(ulong id, Permission fallback = Permission.User)
         {
-            public User(ulong id, Permission level)
-            {
-                this.ID = id;
-                this.OPLevel = level;
-            }
-            public ulong ID;
-            public Permission OPLevel;
-
+            if (Users.ContainsKey(id))
+                return Users[id];
+            return fallback;
         }
+
+        static public void AddUser(ulong id, Permission permission = Permission.User)
+        {
+            if (Users.ContainsKey(id))
+                Users[id] = permission;
+            else
+                Users.Add(id, permission);
+        }
+
         public enum Permission
         {
             SUDO = -1,
@@ -33,8 +38,29 @@ namespace ForgeBot.Tools
             Admin = 1,
             Reserved = 2,
             Editor = 3,
-            Unused = 4
+            Unused = 4,
+            User = 9
 
+        }
+
+        public class UserPermissionLevel : SlashCheckBaseAttribute
+        {
+            public Permission RequiredPermission;
+            /// <summary>
+            /// Checks if the user has the required Operator Level
+            /// </summary>
+            /// <param name="requiredPermission">Minimum Permssion</param>
+            public UserPermissionLevel(Permission requiredPermission)
+            {
+                RequiredPermission = requiredPermission;
+            }
+
+            public override async Task<bool> ExecuteChecksAsync(InteractionContext context)
+            {
+                if (GetUserLevel(context.User.Id) <= RequiredPermission)
+                    return true;
+                return false;
+            }
         }
     }
 }
