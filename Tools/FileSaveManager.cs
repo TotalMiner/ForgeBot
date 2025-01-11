60432ee614d990b83fd5bbe95a534b912547158d
Hayden <hayden@synhayden.com>
2025-01-10 21:25:47 -0700

    Added JSON FileSaveManager

---
+++ Tools/FileSaveManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+
+namespace ForgeBot.Tools
+{
+    public class FileSaveManager
+    {
+        public static void SaveFile(string address, object saveData)
+        {
+            FileStream file = File.Create(address);
+            JsonSerializer.Serialize(file, saveData);
+            file.Close();
+        }
+    }
+}
